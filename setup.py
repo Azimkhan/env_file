e30e954b498b722ae0d7a7c92e6f358caeab9ab3
Azimkhan <me@azimkhan.net>
2016-03-17 14:26:57 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='env_file',
+      version='1.0',
+      description='Load environment variables from file in Python 3 application ',
+      author='Azimkhan Yerzhan',
+      author_email='wanrossen@gmail.com',
+      packages=['env_file'],
+      )
